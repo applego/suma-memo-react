e5bb4fd1e7dc817ee076be92637faf6cf0be1d0f
applego <applegorillappa@gmail.com>
2021-04-14 01:03:41 +0900

    redux-persist-sample

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import App from './components/App'
+import store, { persistor } from './configureStore'
+
+// persistor.purge()
+
+render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
